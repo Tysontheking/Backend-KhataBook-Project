275a199d9dffac2fdd10ddb1f69c1b3edd86debc
Satyam <satyam10253@gmail.com>
2025-04-07 13:02:58 +0530

    Your commit message

--- script.js
+++ script.js
@@ -91,26 +91,86 @@ const fs = require("fs");
 
 // server.listen(3000)
 
+// let express = require("express");
+// let app = express();
+
+// app.use((req, res, next) => {
+//   console.log("middleware");
+//   next();
+// });
+
+// app.get("/", (req, res) => {
+//   res.send("helo");
+// });
+
+// app.get("/profile", (req, res,next) => {
+//           res.sed("profile page");
+//         return next(Error("Something went wrong"))
+// });
+
+// app.use((err, req, res, next) => {
+//         console.error(err.stack)
+//         res.status(500).send('Something broke!')
+//       })
+
+// app.listen(3000);
+
+// const express = require("express");
+// const app = express();
+// const path = require("path");
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.use((req, res, next) => {
+//   console.log("middleware");
+//   next();
+// });
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+// console.log(__dirname + "/public");
+
+// app.get("/about", (req, res, next) => {
+//   res.send("about page");
+//   return next(Error("Something went wrong we don't know"));
+// });
+
+// app.use((err,req,res,next)=>{
+//   console.error(err.stack)
+//   res.status(500).send('Something broke!')
+// })
+
+// app.listen(3000);
+
+//revision
+
 let express = require("express");
+const path = require("path");
 let app = express();
 
-app.use((req, res, next) => {
-  console.log("middleware");
-  next();
-});
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); //add css or js in public folder
 
 app.get("/", (req, res) => {
-  res.send("helo");
+  res.render("index");
 });
 
-app.get("/profile", (req, res,next) => {
-          res.sed("profile page");
-        return next(Error("Something went wrong"))
+app.get("/public", (req, res, next) => {
+  try {
+    res.send("public page");
+  } catch (err) {
+    return next(Error("Something went wrong we don't know"));
+  }
 });
 
 app.use((err, req, res, next) => {
-        console.error(err.stack)
-        res.status(500).send('Something broke!')
-      })
+  console.log(err.stack);
+  res.status(500).send("something went wrong");
+});
 
 app.listen(3000);
